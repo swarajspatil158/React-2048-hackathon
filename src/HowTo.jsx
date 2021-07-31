8b80262c8958a72e2d321fe03bb4267c026465d8
Swaraj Patil <swarajspatil158@gmail.com>
2021-08-01 00:57:00 +0530

    init HoWTo

---
+++ src/HowTo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./HowTo.css"
+
+export default function HowTo() {
+    return (
+        <>
+            <p>Use your arrow keys to move the tiles. Tiles with the same number merge into one when they touch. Add them up to reach 2048!</p>
+            <h1>Start playing →</h1>
+            <header><h1>How to Play</h1></header>
+<main></main>
+<footer>Footer Content — Header.com 2020</footer>
+        </>
+    )
+}
