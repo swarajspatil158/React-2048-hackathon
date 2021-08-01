192d416287c5f49ad62bb9323108a2dfd6c6fe15
Siddharth <siddharth201820@gmail.com>
2021-08-01 19:33:57 +0530

    score fix

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -19,15 +19,19 @@ const gameReducer = (state = initialState, action) => {
 
   if (action.type === "MOVE_LEFT") {
     [newBoard, mergeScore] = moveLeft(action.payload);
+    newScore = state.score + mergeScore;
     newState = { ...state, board: newBoard, score: newScore };
   } else if (action.type === "MOVE_RIGHT") {
     [newBoard, mergeScore] = moveRight(action.payload);
+    newScore = state.score + mergeScore;
     newState = { ...state, board: newBoard, score: newScore };
   } else if (action.type === "MOVE_UP") {
     [newBoard, mergeScore] = moveUp(action.payload);
+    newScore = state.score + mergeScore;
     newState = { ...state, board: newBoard, score: newScore };
   } else if (action.type === "MOVE_DOWN") {
     [newBoard, mergeScore] = moveDown(action.payload);
+    newScore = state.score + mergeScore;
     newState = { ...state, board: newBoard, score: newScore };
   }
   newScore = state.score + mergeScore;
