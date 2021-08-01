7f8deda2942a4fe10b19c0fa5dd8187271ac59ad
abhaygupta08 <abhay.19201@knit.ac.in>
2021-08-01 12:18:54 +0530

    landing page pre-final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-// import Landing from './components/Landing.jsx';
-// import "./components/Landing.css";
 
 import Landing1 from './components/Landing1.jsx';
 import "./components/Landing1.css";
--- src/components/Landing1.jsx
+++ src/components/Landing1.jsx
@@ -1,16 +1,29 @@
 const Landing1 = () => {
+    
+    function showHeart() {
+        const heart = document.createElement("div");
+        heart.innerText = ["2", "4", "8", "16", "32", "64"][Math.floor(Math.random() * 6)];
+        heart.classList.add("heart");
+        heart.style.transform = `scale(${Math.random() * 5})`;
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() + 2 + 3 + "s";
+        document.body.appendChild(heart);
+        setTimeout(() => {
+            heart.remove();
+        }, 4000);
+    }
+
+    setInterval(() => {
+        showHeart();
+    }, 5000);
+
     return(<>
         <div className='ripple-background'>
-            <div className='circle xxlarge shade1'></div>
-            <div className='circle xlarge shade2'></div>
-            <div className='circle large shade3'></div>
-            <div className='circle medium shade4'></div>
-            <div className='circle small shade5'></div>
-        </div>
+            </div>
         <div className="pageContent">
             <header>
                 <span>The Mind Game</span>
-                <h1>2048 GAME</h1>
+                <h1>2048</h1>
             </header>
 
             <div className="mainTab">
