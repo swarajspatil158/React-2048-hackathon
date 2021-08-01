58d0788d3337edda4caf03fcfe4fcccfa1fa63b0
Siddharth <siddharth201820@gmail.com>
2021-08-01 19:32:03 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import GameBoard from "./components/GameBoard";
 import Score from "./components/Score";
-import HowTo from "./HowTo";
+import HowTo from "./components/HowTo";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Score />
-      <GameBoard />
-      <HowTo />
+      <Switch>
+        <Route
+          path="/game"
+          component={() => (
+            <>
+              <Score />
+              <GameBoard />
+            </>
+          )}
+        />
+        <Route exact path="/" component={HowTo} />
+      </Switch>
     </div>
   );
 }
