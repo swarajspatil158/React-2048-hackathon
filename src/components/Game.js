e5dcc410f062972cb792510d4c811ffbd3020e59
abhaygupta08 <abhay.19201@knit.ac.in>
2021-08-02 12:00:49 +0530

    day3

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import { useDispatch } from "react-redux";
 import { newGame } from "../actions/index";
 
 import GameBoard from "./GameBoard";
 import Score from "./Score";
-
+import HowToModal from "./HowToModal.jsx";
+import "./HowToModal.css";
 const Game = () => {
+  const [ModalIsOpen, setModalIsOpen] = useState(false);
+  // const [BoardLocked, setBoardLocked] = useState(false);
   // dispatch function for creating newGame
   const dispatch = useDispatch();
   return (
     <div className="game">
+      <button onClick={()=>{setModalIsOpen(true)}}>How to Play</button>
+      {ModalIsOpen?
+      (<HowToModal setModalIsOpen={setModalIsOpen}></HowToModal>)
+      :null
+      }
       <button onClick={() => dispatch(newGame())}>NewGame</button>
       <Score />
       <GameBoard />
