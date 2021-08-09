e141f62778922d721c0b177defa1076014d39297
Swaraj Patil <swarajspatil158@gmail.com>
2021-08-09 22:31:00 +0530

    Deploy build

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import gameReducer from "./gameReducers";
-
-// combining if there is more than one reducer
-const rootReducer = combineReducers({ gameBoard: gameReducer });
-
-export default rootReducer;
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import rootReducer from "./reducers/index";
-import { createStore } from "redux";
-
-// providing the root reducer for redux to the react app
-const store = createStore(rootReducer);
-
-export default store;
